de077a3341e84c4da270099e448b39c93e144967
Davy ELLE MBENG <davy.ellembeng@gmail.com>
2022-03-23 00:59:31 +0100

    Normal Drug test

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,33 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  it("should return null drug if negative benefit", () => {
+    expect(
+      new Pharmacy([new Drug("Normal Drug", 2, -1)]).updateBenefitValue()
+    ).toEqual([null]);
+  });
+
+  it("should return null drug if benefit greater than 50", () => {
+    expect(
+      new Pharmacy([new Drug("Normal Drug", 2, 51)]).updateBenefitValue()
+    ).toEqual([null]);
+  });
+
+  it("should decrease normal drug benefit and expiresIn", () => {
+    expect(
+      new Pharmacy([new Drug("Normal Drug", 3, 50)]).updateBenefitValue()
+    ).toEqual([new Drug("Normal Drug", 2, 49)]);
+  });
+
+  it("should not decrease normal drug 0 benefit but expiresIn", () => {
+    expect(
+      new Pharmacy([new Drug("Normal Drug", -2, 0)]).updateBenefitValue()
+    ).toEqual([new Drug("Normal Drug", -3, 0)]);
+  });
+
+  it("should decrease normal drug benefit twice fast as expiration date passed", () => {
+    expect(
+      new Pharmacy([new Drug("Normal Drug", -2, 10)]).updateBenefitValue()
+    ).toEqual([new Drug("Normal Drug", -3, 8)]);
   });
 });

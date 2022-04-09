841166fdfc897e05e9db32435127ea80db45aa1a
MaxGenest <maxime@olybe.com>
2022-04-09 10:48:59 +0200

    create a drug.js file

--- pharmacy.js
+++ pharmacy.js
@@ -1,15 +1,8 @@
-export class Drug {
-  constructor(name, expiresIn, benefit) {
-    this.name = name;
-    this.expiresIn = expiresIn;
-    this.benefit = benefit;
-  }
-}
-
 export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
+
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
       if (

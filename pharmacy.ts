fdec08938e97b4744f3125e4081cdc5963e430cc
Flavien VALENTIN <fvalentin@oui.sncf>
2022-04-10 14:59:48 +0200

    refactor: extract magic strings/numbers

--- pharmacy.ts
+++ pharmacy.ts
@@ -1,44 +1,53 @@
 import { Drug } from "./drug";
 
+const HERBAL_TEA = "Herbal Tea";
+const FERVEX = "Fervex";
+const MAGIC_PILL = "Magic Pill";
+
+const MAX_BENEFIT = 50;
+const DOUBLE_BENEFIT_UNDER_DAY_EXPIRATION = 10;
+const TRIPLE_BENEFIT_UNDER_DAY_EXPIRATION = 5;
+
 export class Pharmacy {
   constructor(private readonly drugs: Drug[] = []) {}
 
   updateBenefitValue(): Drug[] {
     for (let i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
+      if (this.drugs[i].name != HERBAL_TEA && this.drugs[i].name != FERVEX) {
         if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
+          if (this.drugs[i].name != MAGIC_PILL) {
             this.drugs[i].benefit = this.drugs[i].benefit - 1;
           }
         }
       } else {
-        if (this.drugs[i].benefit < 50) {
+        if (this.drugs[i].benefit < MAX_BENEFIT) {
           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
+          if (this.drugs[i].name == FERVEX) {
+            if (
+              this.drugs[i].expiresIn <= DOUBLE_BENEFIT_UNDER_DAY_EXPIRATION
+            ) {
+              if (this.drugs[i].benefit < MAX_BENEFIT) {
                 this.drugs[i].benefit = this.drugs[i].benefit + 1;
               }
             }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
+            if (
+              this.drugs[i].expiresIn <= TRIPLE_BENEFIT_UNDER_DAY_EXPIRATION
+            ) {
+              if (this.drugs[i].benefit < MAX_BENEFIT) {
                 this.drugs[i].benefit = this.drugs[i].benefit + 1;
               }
             }
           }
         }
       }
-      if (this.drugs[i].name != "Magic Pill") {
+      if (this.drugs[i].name != MAGIC_PILL) {
         this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       }
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
+        if (this.drugs[i].name != HERBAL_TEA) {
+          if (this.drugs[i].name != FERVEX) {
             if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
+              if (this.drugs[i].name != MAGIC_PILL) {
                 this.drugs[i].benefit = this.drugs[i].benefit - 1;
               }
             }
@@ -47,7 +56,7 @@ export class Pharmacy {
               this.drugs[i].benefit - this.drugs[i].benefit;
           }
         } else {
-          if (this.drugs[i].benefit < 50) {
+          if (this.drugs[i].benefit < MAX_BENEFIT) {
             this.drugs[i].benefit = this.drugs[i].benefit + 1;
           }
         }

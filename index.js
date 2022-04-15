0e4d3b1cdb1fc50290f23b2ce7e1bd3d6ab682ae
gRaimbault <raimbault.guillaume@gmail.com>
2022-04-15 21:09:27 +0200

    refactor of the updateBenefitValue method, implementation of the Dafalgan drug and small changes to how index.js output its file for testing purpose

--- index.js
+++ index.js
@@ -8,15 +8,23 @@ const drugs = [
   new Drug("Magic Pill", 15, 40)
 ];
 const trial = new Pharmacy(drugs);
-
 const log = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
   log.push(JSON.stringify(trial.updateBenefitValue()));
 }
 
+const stringLog = log.toString();
+let multipleLineLog = "";
+for(let i = 0; i < stringLog.length; i++){
+  multipleLineLog += stringLog[i];
+  if(stringLog[i]== ']') {
+    multipleLineLog += "\n";
+  }
+}
+
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log.toString(), err => {
+fs.writeFile("output.txt", multipleLineLog, err => {
   if (err) {
     console.log("error");
   } else {

9d269c0ecd4e09008469c2db90ab76d8338fb0da
Julien Auvray <julien.auvray@ateliersduroi.com>
2022-04-27 00:09:59 +0200

    refactor: change for use new updateDrug method on each drug

--- pharmacy.test.ts
+++ pharmacy.test.ts
@@ -1,10 +1,15 @@
-import { Drug } from "./drug";
+import { Doliprane } from "./drugs/doliprane";
 import { Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
+
+  it('should be true', () => {
+    expect(true).toBeTruthy();
+  })
+
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
+    expect(new Pharmacy([new Doliprane(2, 3)]).updateBenefitValue()).toEqual(
+      [new Doliprane(1, 2)]
     );
   });
 });

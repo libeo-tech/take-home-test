d43f722309ed21f83ca50e401f0864082ffa8e62
bvalette <bvalette@student.42.fr>
2022-05-13 11:09:07 +0200

    feat(drugs): sets up the basic logic for each derived drug

--- src/drug.ts
+++ src/drug.ts
@@ -6,20 +6,54 @@ export class Drug {
   constructor(name: string, expiresIn: number, benefit: number) {
     this.name = name;
     this.expiresIn = expiresIn;
-    this.benefit = (benefit > 50) ? 50 : ((benefit < 0) ? 0 : benefit);
+    this.benefit = benefit > 50 ? 50 : benefit < 0 ? 0 : benefit;
   }
 
-	updateOwnBenefitValue() {}
+  hasExpired() {
+    return this.expiresIn <= 0;
+  }
+
+  updateBenefit(variation: number) {
+    const newBenefit = this.benefit + variation;
+    this.benefit = newBenefit > 50 ? 50 : newBenefit < 0 ? 0 : newBenefit;
+  }
+  updateOwnBenefitValue() {
+    const benefitVariation = this.hasExpired() ? -2 : -1;
+    this.updateBenefit(benefitVariation);
+    this.expiresIn--;
+  }
 }
 
 export class HerbalTea extends Drug {
-	updateOwnBenefitValue() {}
+  updateOwnBenefitValue() {
+    const benefitVariation = this.hasExpired() ? 2 : 1;
+    this.updateBenefit(benefitVariation);
+    this.expiresIn--;
+  }
 }
 
 export class Fervex extends Drug {
-	updateOwnBenefitValue() {}
+  updateOwnBenefitValue() {
+    if (this.hasExpired()) {
+      this.benefit = 0;
+    } else {
+      let benefitVariation = 1;
+      if (this.expiresIn > 5 && this.expiresIn <= 10) {
+        benefitVariation = 2;
+      } else if (this.expiresIn > 0 && this.expiresIn <= 5) {
+        benefitVariation = 3;
+      }
+      this.updateBenefit(benefitVariation);
+    }
+    this.expiresIn--;
+  }
 }
 
 export class MagicPill extends Drug {
-	updateOwnBenefitValue() {}
+  /**
+   * This method is intentionally left empty because the behavior of this class
+   * is:
+   *  - "Magic Pill" never expires nor decreases in Benefit.
+   */
+  updateOwnBenefitValue() {}
 }

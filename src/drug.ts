055268be9e6040b19ee3873bb6635c9eb20731d5
bvalette <bvalette@student.42.fr>
2022-05-13 10:27:27 +0200

    refactor(drug): implements benefit specification, value [0, 50]

--- src/drug.ts
+++ src/drug.ts
@@ -6,6 +6,6 @@ export class Drug {
   constructor(name: string, expiresIn: number, benefit: number) {
     this.name = name;
     this.expiresIn = expiresIn;
-    this.benefit = benefit;
+    this.benefit = (benefit > 50) ? 50 : ((benefit < 0) ? 0 : benefit);
   }
 }

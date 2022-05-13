d3a163a0a2be45ce36921706bca4bdf252e40721
bvalette <bvalette@student.42.fr>
2022-05-13 10:28:09 +0200

    feat(drug): add derived drugs

--- src/drug.ts
+++ src/drug.ts
@@ -8,4 +8,18 @@ export class Drug {
     this.expiresIn = expiresIn;
     this.benefit = (benefit > 50) ? 50 : ((benefit < 0) ? 0 : benefit);
   }
+
+	updateOwnBenefitValue() {}
+}
+
+export class HerbalTea extends Drug {
+	updateOwnBenefitValue() {}
+}
+
+export class Fervex extends Drug {
+	updateOwnBenefitValue() {}
+}
+
+export class MagicPill extends Drug {
+	updateOwnBenefitValue() {}
 }

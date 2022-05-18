cb1468b18756b0f8656789ab84a754196b11c349
Matt K <matthedud@hotmail.com>
2022-05-18 09:44:22 +0200

    change updateDrugValues for compatibility with other drugs

--- drugs.js
+++ drugs.js
@@ -1,21 +1,37 @@
-
 export class Drug {
-    constructor(name, expiresIn, benefit) {
-      this.name = name;
-      this.expiresIn = expiresIn;
-      this.benefit = benefit;
+  constructor(name, expiresIn, benefit) {
+    this.name = name;
+    this.expiresIn = expiresIn;
+    this.benefit = benefit;
+    this.benefitUpdateValue = -1;
+    this.benefitMinValue = 0;
+    this.benefitMaxValue = 50;
+  }
+
+  updateDrugValues() {
+    // update benefit normaly and expiring date if it hasn't reached 0 yet
+    if (this.expiresIn >= 1) {
+      this.expiresIn--;
+      this.benefit += this.benefitUpdateValue;
+    } else {
+      this.benefit += this.benefitUpdateValue * 2; // update the benefits twice as fast after the expiring date is passed
     }
 
-    updateDrugValues () {
-        if(this.expiresIn >= 1) {
-            this.expiresIn--
-            if (this.benefit >= 1) this.benefit--
-        }
-        else {
-            if (this.benefit >= 2)  this.benefit -= 2
-            else if (this.benefit >= 1)  this.benefit --
-        }
-        return this
+    // correct the benefit value if it went below the min value
+    if (this.benefit < this.benefitMinValue) {
+      this.benefit = this.benefitMinValue;
     }
+    // correct the benefit value if it went over the max value
+    if (this.benefit > this.benefitMaxValue) {
+      this.benefit = this.benefitMaxValue;
+    }
+    return this;
   }
-  
+}
+
+export class HerbalTea extends Drug {
+  constructor(name, expiresIn, benefit) {
+    super(name, expiresIn, benefit);
+    this.benefitUpdateValue = 1;
+  }
+}

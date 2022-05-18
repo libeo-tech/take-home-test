14af0e5542b5543b1fda1fcd9c4a5bf7e9325979
Matt K <matthedud@hotmail.com>
2022-05-18 11:15:34 +0200

    Refactor pharmacy to add new feature

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,17 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { Drug } from "./drugs";
+import { Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
+    const updatedDrug = new Drug("test", 1, 2);
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
+      [
+        {
+          name: updatedDrug.name,
+          expiresIn: updatedDrug.expiresIn,
+          benefit: updatedDrug.benefit
+        }
+      ]
     );
   });
 });

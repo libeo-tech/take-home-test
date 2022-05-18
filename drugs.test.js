a6f4d4767b61184ce4d27d46490009876ca338ec
Matt K <matthedud@hotmail.com>
2022-05-18 09:49:07 +0200

    create class and tests for herbal tea

--- drugs.test.js
+++ drugs.test.js
@@ -1,4 +1,4 @@
-import { Drug } from "./drugs";
+import { Drug, HerbalTea } from "./drugs";
 
 describe("Drugs", () => {
   it("should decrease the benefit and expiresIn by one each", () => {
@@ -24,6 +24,34 @@ describe("Drugs", () => {
   it("should not change the values of the benifit and expiresIn", () => {
     expect(new Drug("test", 0, 0).updateDrugValues()).toEqual(
       new Drug("test", 0, 0)
+    );
+  });
+});
+
+describe("HerbalTea", () => {
+  it("should decrease the benefit and increase expiresIn by one each", () => {
+    expect(new HerbalTea("test", 2, 3).updateDrugValues()).toEqual(
+      new HerbalTea("test", 1, 4)
+    );
+  });
+  it("should only increase the benefit by 2", () => {
+    expect(new HerbalTea("test", 0, 3).updateDrugValues()).toEqual(
+      new HerbalTea("test", 0, 5)
+    );
+  });
+  it("should only increase the benefit by 1", () => {
+    expect(new HerbalTea("test", 0, 49).updateDrugValues()).toEqual(
+      new HerbalTea("test", 0, 50)
+    );
+  });
+  it("should only decrease the expiresIn by 1", () => {
+    expect(new HerbalTea("test", 1, 50).updateDrugValues()).toEqual(
+      new HerbalTea("test", 0, 50)
+    );
+  });
+  it("should not change the values of the benifit and expiresIn", () => {
+    expect(new HerbalTea("test", 0, 50).updateDrugValues()).toEqual(
+      new HerbalTea("test", 0, 50)
     );
   });
 });

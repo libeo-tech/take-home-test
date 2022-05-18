a4d6931df8f8aa02075bba3505a01c8a5cd14597
Matt K <matthedud@hotmail.com>
2022-05-18 10:00:22 +0200

    move the correction of the benefit value in a separate method: correctBenefits()

--- drugs.js
+++ drugs.js
@@ -16,7 +16,11 @@ export class Drug {
     } else {
       this.benefit += this.benefitUpdateValue * 2; // update the benefits twice as fast after the expiring date is passed
     }
+    this.correctBenefits();
+    return this;
+  }
 
+  correctBenefits() {
     // correct the benefit value if it went below the min value
     if (this.benefit < this.benefitMinValue) {
       this.benefit = this.benefitMinValue;
@@ -25,7 +29,6 @@ export class Drug {
     if (this.benefit > this.benefitMaxValue) {
       this.benefit = this.benefitMaxValue;
     }
-    return this;
   }
 }
 
@@ -37,7 +40,30 @@ export class HerbalTea extends Drug {
 }
 
 export class MagicPill extends Drug {
+  // the values of Magis pill doesn't change over time
   updateDrugValues() {
     return this;
   }
 }
+
+// export class Fervex extends Drug {
+//   updateDrugValues() {
+//     // update benefit normaly and expiring date if it hasn't reached 0 yet
+//     if (this.expiresIn >= 1) {
+//       this.expiresIn--;
+//       this.benefit += this.benefitUpdateValue;
+//     } else {
+//       this.benefit += this.benefitUpdateValue * 2; // update the benefits twice as fast after the expiring date is passed
+//     }
+
+//     // correct the benefit value if it went below the min value
+//     if (this.benefit < this.benefitMinValue) {
+//       this.benefit = this.benefitMinValue;
+//     }
+//     // correct the benefit value if it went over the max value
+//     if (this.benefit > this.benefitMaxValue) {
+//       this.benefit = this.benefitMaxValue;
+//     }
+//     return this;
+//   }
+// }

0e6e4720e48f669194c9bf6a426a1b077db194db
paul-louis <p.louisbernard.dev@gmail.com>
2022-05-26 15:10:21 +0200

    add some test in ts

---
+++ test/pharmacy.test.ts
@@ -0,0 +1,12 @@
+import Pharmacy from '../src/pharmacy';
+import Drug from '../src/Model/Drug';
+
+describe('Pharmacy', () => {
+  it('should decrease the benefit and expiresIn', () => {
+    expect(new Pharmacy([new Drug('test', 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug('test', 1, 2)],
+    );
+  });
+});
+
+

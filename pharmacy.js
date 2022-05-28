16b2188500322d6df3ccf7d913c54f06d12ecbad
Nicolas Sauvage <nicolas.sauvage@talan.com>
2022-05-28 15:57:08 +0200

    refacto: extract herbal tea ; add script for non regression

--- pharmacy.js
+++ pharmacy.js
@@ -16,52 +16,64 @@ export class Pharmacy {
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
-      const drug = this.drugs[i];
-      if (drug.name != herbalTea && drug.name != fervex) {
-        if (drug.benefit > 0) {
-          if (drug.name != magicPill) {
-            drug.benefit = drug.benefit - 1;
-          }
-        }
-      } else {
-        if (drug.benefit < 50) {
-          drug.benefit = drug.benefit + 1;
-          if (drug.name == fervex) {
-            if (drug.expiresIn < 11) {
-              if (drug.benefit < 50) {
-                drug.benefit = drug.benefit + 1;
-              }
-            }
-            if (drug.expiresIn < 6) {
-              if (drug.benefit < 50) {
-                drug.benefit = drug.benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (drug.name != magicPill) {
-        drug.expiresIn = drug.expiresIn - 1;
-      }
+      this.updateDrug(this.drugs[i]);
+    }
+
+    return this.drugs;
+  }
+
+  updateDrug(drug) {
+    if (drug.name == herbalTea) {
+      drug.expiresIn--;
       if (drug.expiresIn < 0) {
-        if (drug.name != herbalTea) {
-          if (drug.name != fervex) {
-            if (drug.benefit > 0) {
-              if (drug.name != magicPill) {
-                drug.benefit = drug.benefit - 1;
-              }
+        drug.benefit += 2;
+      } else {
+        drug.benefit++;
+      }
+      if (drug.benefit > 50) {
+        drug.benefit = 50;
+      }
+      return;
+    }
+
+    if (drug.name != fervex) {
+      if (drug.benefit > 0) {
+        if (drug.name != magicPill) {
+          drug.benefit = drug.benefit - 1;
+        }
+      }
+    } else {
+      if (drug.benefit < 50) {
+        drug.benefit = drug.benefit + 1;
+        if (drug.name == fervex) {
+          if (drug.expiresIn < 11) {
+            if (drug.benefit < 50) {
+              drug.benefit = drug.benefit + 1;
             }
-          } else {
-            drug.benefit = drug.benefit - drug.benefit;
           }
-        } else {
-          if (drug.benefit < 50) {
-            drug.benefit = drug.benefit + 1;
+          if (drug.expiresIn < 6) {
+            if (drug.benefit < 50) {
+              drug.benefit = drug.benefit + 1;
+            }
           }
         }
       }
     }
-
-    return this.drugs;
+    if (drug.name != magicPill) {
+      drug.expiresIn = drug.expiresIn - 1;
+    }
+    if (drug.expiresIn < 0) {
+      if (drug.name != herbalTea) {
+        if (drug.name != fervex) {
+          if (drug.benefit > 0) {
+            if (drug.name != magicPill) {
+              drug.benefit = drug.benefit - 1;
+            }
+          }
+        } else {
+          drug.benefit = drug.benefit - drug.benefit;
+        }
+      }
+    }
   }
 }

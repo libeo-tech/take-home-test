bb20d1bb00dec2dd8616c7df7393fd6a4fcabfa3
jeremyben <jeremy.bensimon@skeepers.io>
2023-12-19 22:19:13 +0100

    chore: fix lint errors

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,7 +1,5 @@
 import { Drug, Pharmacy } from "./pharmacy.js";
 
-
-
 describe("Pharmacy and regular drugs", () => {
   it("should decrease the benefit and expiresIn", () => {
     const pharmacy = new Pharmacy([
@@ -35,42 +33,41 @@ describe("Pharmacy and regular drugs", () => {
   });
 });
 
-
 describe("Special drugs", () => {
-  it('should not update Magic Pill', () => {
-    const drug = new Drug('Magic Pill', 5, 10);
+  it("should not update Magic Pill", () => {
+    const drug = new Drug("Magic Pill", 5, 10);
     updateDrugBy(drug, 5);
-    
+
     expect(drug.expiresIn).toBe(5);
     expect(drug.benefit).toBe(10);
-  })
+  });
 
-  it('should increase benefit for Herbal Tea', () => {
-    const drug = new Drug('Herbal Tea', 5, 10);
+  it("should increase benefit for Herbal Tea", () => {
+    const drug = new Drug("Herbal Tea", 5, 10);
     updateDrugBy(drug, 2);
 
     expect(drug.expiresIn).toBe(3);
     expect(drug.benefit).toBe(12);
-  })
+  });
 
-  it('should increase benefit twice for Herbal Tea after expiration', () => {
-    const drug = new Drug('Herbal Tea', 1, 10);
+  it("should increase benefit twice for Herbal Tea after expiration", () => {
+    const drug = new Drug("Herbal Tea", 1, 10);
     updateDrugBy(drug, 3);
 
     expect(drug.expiresIn).toBe(-2);
     expect(drug.benefit).toBe(15);
-  })
+  });
 
-  it('should not have a benefit over 50 for Herbal Tea', () => {
-    const drug = new Drug('Herbal Tea', 1, 49);
+  it("should not have a benefit over 50 for Herbal Tea", () => {
+    const drug = new Drug("Herbal Tea", 1, 49);
     updateDrugBy(drug, 2);
 
     expect(drug.expiresIn).toBe(-1);
     expect(drug.benefit).toBe(50);
-  })
+  });
 
-  it('should increase benefit accordingly for Fervex', () => {
-    const drug = new Drug('Fervex', 6, 10);
+  it("should increase benefit accordingly for Fervex", () => {
+    const drug = new Drug("Fervex", 6, 10);
 
     drug.updateBenefit();
     expect(drug.expiresIn).toBe(5);
@@ -87,17 +84,16 @@ describe("Special drugs", () => {
     drug.updateBenefit();
     expect(drug.expiresIn).toBe(-1);
     expect(drug.benefit).toBe(0);
-  })
+  });
 
-  it('should not have a benefit over 50 for Fervex', () => {
-    const drug = new Drug('Herbal Tea', 10, 45);
+  it("should not have a benefit over 50 for Fervex", () => {
+    const drug = new Drug("Herbal Tea", 10, 45);
     updateDrugBy(drug, 8);
 
     expect(drug.expiresIn).toBe(2);
     expect(drug.benefit).toBe(50);
-  })
-})
-
+  });
+});
 
 /**
  * @param {Drug} drug
@@ -106,7 +102,7 @@ describe("Special drugs", () => {
  */
 function updateDrugBy(drug, times) {
   for (let i = 0; i < times; i++) {
-    drug.updateBenefit()
+    drug.updateBenefit();
   }
-  return drug
+  return drug;
 }

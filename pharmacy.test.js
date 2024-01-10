a18cb8d7330ab2de55f379e02318e7565512eeb2
davy.tran <davytran1993@gmail.com>
2024-01-10 19:46:17 +0100

    Refactor the unit tests

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,33 +1,59 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  describe("Other drugs", () => {
+    it("should decrease the benefit and expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 2)]);
+    });
+
+    it("should unchange benefit and decrease expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("test", 0, 0)]).updateBenefitValue()
+      ).toEqual([new Drug("test", -1, 0)]);
+    });
   });
 
-  it("should unchange benefit and decrease expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 0, 0)]).updateBenefitValue()).toEqual(
-      [new Drug("test", -1, 0)]
-    );
+  describe("Herbal Tea", () => {
+    it("should increase the benefit and decrease expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", 1, 4)]);
+    });
+
+    it("should increase the benefit by 2 and decrease expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", 0, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", -1, 5)]);
+    });
+
+    it("should not increase the benefit", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", 0, 50)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", -1, 50)]);
+    });
   });
 
-  it("should increase the benefit by 1 and decrease expiresIn", () => {
-    expect(
-      new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", 1, 4)]);
+  describe("Magic Pill", () => {
+    it("should do nothing", () => {
+      expect(
+        new Pharmacy([new Drug("Magic Pill", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("Magic Pill", 2, 3)]);
+    });
   });
 
-  it("should increase the benefit by 2 and decrease expiresIn", () => {
-    expect(
-      new Pharmacy([new Drug("Herbal Tea", 0, 3)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", -1, 5)]);
-  });
+  describe("Fervex", () => {
+    it("should increase the benefit by 2 and decrease expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", 1, 5)]);
+    });
 
-  it("should do nothing", () => {
-    expect(
-      new Pharmacy([new Drug("Magic Pill", 2, 3)]).updateBenefitValue()
-    ).toEqual([new Drug("Magic Pill", 2, 3)]);
+    it("should not increase the benefit", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 0, 50)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", -1, 50)]);
+    });
   });
 });

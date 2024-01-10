962f4f760dbb083394ca314add1ffa33e029ee58
davy.tran <davytran1993@gmail.com>
2024-01-10 19:25:17 +0100

    Refactoring the pharmacy by adding inherit classes

--- pharmacy.js
+++ pharmacy.js
@@ -1,64 +1,81 @@
 export class Drug {
+  /**
+   * Drug class.
+   * @param {string} name name of the drug.
+   * @param {number} expiresIn value which denotes the number of days we have until the item expires.
+   * @param {number} benefit value which denotes how powerful the drug is.
+   */
   constructor(name, expiresIn, benefit) {
     this.name = name;
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
+
+  /**
+   * Set the value of benefit.
+   * @param {number} benefit value which denotes how powerful the drug is.
+   */
+  updateBenefit() {
+    if (this.benefit > 0) {
+      --this.benefit;
+    }
+  }
+
+  /**
+   * Update the value of expiresIn.
+   */
+  updateExpiresIn() {
+    --this.expiresIn;
+  }
 }
 
-export class Pharmacy {
-  constructor(drugs = []) {
-    this.drugs = drugs;
+class HerbalTea extends Drug {
+  updateBenefit() {
+    if (this.benefit < 50) {
+      this.benefit = this.expiresIn < 0 ? this.benefit + 2 : this.benefit + 1;
+    }
   }
-  updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
+}
+
+class MagicPill extends Drug {
+  updateBenefit() {}
+  updateExpiresIn() {}
+}
+
+class Fervex extends Drug {
+  updateBenefit() {
+    if (this.benefit < 50) {
+      this.benefit = this.expiresIn <= 10 ? this.benefit + 2 : this.benefit + 1;
+    }
+  }
+}
+
+const drugsClassHandler = {
+  "Herbal Tea": drug => new HerbalTea(drug.name, drug.expiresIn, drug.benefit),
+  "Magic Pill": drug => new MagicPill(drug.name, drug.expiresIn, drug.benefit),
+  Fervex: drug => new Fervex(drug.name, drug.expiresIn, drug.benefit)
+};
+
+export class Pharmacy {
+  /**
+   * Pharmacy class.
+   * @param {Drug[]} drugs List of drugs.
+   */
+  constructor(drugs = []) {
+    this.drugs = [];
+    for (let drug of drugs) {
+      if (drug.name in drugsClassHandler) {
+        this.drugs.push(drugsClassHandler[drug.name](drug));
       } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
+        this.drugs.push(drug);
       }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
+    }
+  }
+
+  updateBenefitValue() {
+    for (const drug of this.drugs) {
+      drug.updateBenefit(drug.benefit - 1);
+      drug.updateExpiresIn(drug.expiresIn - 1);
     }
 
     return this.drugs;
